6936f1cee4f3aa56f471f0b54b1945038dd0c40d
Shahzaib Rahim <shahzaibrahim7@gmail.com>
2017-05-30 23:50:01 +0500

    Update config.py

--- dbooad/config.py
+++ dbooad/config.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 engine = create_engine('oracle://k142214:srseven71@127.0.0.1:1521/XE', echo=True) #Network parameters for database connection
-connection = engine.connect()
+connection = engine.connect() #connect to database
 result = connection.execute() #execute SQL queries here
 
